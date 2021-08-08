0e2123ae563f725ee24de1de645c4aad5d9226c9
Rumira Daksith <rumiradaksith123@gmail.com>
2021-08-08 17:48:51 +0530

    Websocket implemented

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+var osu = require('node-os-utils')
+
+
+const { info } = require('console');
+
+
+
+
 const port = 3000;
 
-
 app.get('/', (req, res) => {
+
     res.sendFile(__dirname + '/index.html');
 });
 
@@ -13,8 +22,34 @@ io.on('connection', (socket) => {
     socket.on('chat message', msg => {
         console.log("Message Received");
         io.emit('chat message', msg);
+
     });
 });
+
+app.get('/getmsgs', (req, res) => {
+
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/spec', (req, res) => {
+    var cpu = osu.cpu
+
+    cpu.usage()
+        .then(info => {
+            console.log(info)
+        })
+    res.status(200).send("usage");
+});
+
+setInterval(function() {
+    var cpu = osu.cpu
+
+    cpu.usage()
+        .then(info => {
+            console.log(info)
+        });
+
+}, 2000);
 
 http.listen(port, () => {
     console.log(`Socket.IO server running at http://localhost:${port}/`);
